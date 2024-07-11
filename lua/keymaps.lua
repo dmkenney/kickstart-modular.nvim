6403b6328995f94cc7eb704e97e718ad027c3f7f
Dylan Kenney <dylan@dkenney.com>
2024-07-10 22:58:25 -0400

    WIP Neotree

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -34,6 +34,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit All' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
