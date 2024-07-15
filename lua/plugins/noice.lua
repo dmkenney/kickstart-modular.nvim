1c6f67e3d17d02115564d709678bf91a1605a81b
Dylan Kenney <dylan@dkenney.com>
2024-07-15 12:18:25 -0400

    noice and bufferline

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,11 +2,7 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
   },
   opts = {
@@ -19,15 +15,8 @@ return {
     },
     routes = {
       {
-        filter = {
-          event = "msg_show",
-          any = {
-            { find = "%d+L, %d+B" },
-            { find = "; after #%d+" },
-            { find = "; before #%d+" },
-          },
-        },
-        view = "mini",
+        view = "notify",
+        filter = { event = "msg_showmode" },
       },
     },
     presets = {
